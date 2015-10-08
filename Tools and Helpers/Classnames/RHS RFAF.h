8a612f7bfe4701dc48fdb342f0c05816db3c4294
WolfkillArcadia <WolfkillArcadia@gmail.com>
2015-10-07 21:21:16 -0400

    updates

---
+++ Tools and Helpers/Classnames/RHS RFAF.h
@@ -0,0 +1,363 @@
+_rhs_weapons = [
+	"Binocular",
+	"rhs_tr8_periscope",
+	"rhs_tr8_periscope_pip",
+	"rhs_weap_tr8",
+	"rhs_pdu4",
+
+	"rhs_weap_ak74m",
+	"rhs_weap_ak74m_plummag",
+	"rhs_weap_ak74m_camo",
+	"rhs_weap_ak74m_desert",
+	"rhs_weap_ak74m_gp25",
+	"rhs_weap_ak74m_folded",
+	"rhs_weap_ak74m_2mag",
+	"rhs_weap_ak74m_2mag_camo",
+	"rhs_weap_ak74m_npz",
+	"rhs_weap_ak74m_plummag_npz",
+	"rhs_weap_ak74m_camo_npz",
+	"rhs_weap_ak74m_desert_npz",
+	"rhs_weap_ak74m_2mag_npz",
+	"rhs_weap_ak74m_2mag_camo_npz",
+	"rhs_weap_ak74m_gp25_npz",
+
+	"rhs_weap_akm",
+	"rhs_weap_akms",
+
+	"rhs_weap_akm_gp25",
+	"rhs_weap_akms_gp25",
+
+	"rhs_weap_ak103",
+	"rhs_weap_ak103_npz",
+	"rhs_weap_ak103_1",
+	"rhs_weap_ak103_2",
+
+	"rhs_weap_svdp",
+	"rhs_weap_svdp_wd",
+	"rhs_weap_svds",
+	"rhs_weap_svdp_npz",
+	"rhs_weap_svdp_wd_npz",
+	"rhs_weap_svds_npz",
+
+	"rhs_weap_pkm",
+	"rhs_weap_pkp",
+
+	"rhs_weap_rpg26",
+	"rhs_weap_rshg2",
+	"rhs_weap_rpg7",
+	"rhs_weap_igla",
+	"rhs_weap_rsp30_white",
+	"rhs_weap_rsp30_green",
+	"rhs_weap_rsp30_red",
+
+
+	"rhs_weap_pya",
+	"rhs_weap_makarov_pmm",
+
+
+	//ai variants
+	"rhs_weap_ak74m_dtk",
+	"rhs_weap_ak74m_pkas",
+	"rhs_weap_ak74m_1p63",
+	"rhs_weap_ak74m_pso1m2",
+	"rhs_weap_ak74m_1p29",
+	"rhs_weap_ak74m_camo_dtk",
+	"rhs_weap_ak74m_npz_dtk",
+	"rhs_weap_ak103_dtk",
+	"rhs_weap_ak103_npz_dtk",
+	"rhs_weap_ak103_1_dtk",
+	"rhs_weap_ak103_2_dtk",
+	"rhs_weap_ak74m_folded_dtk",
+	"rhs_weap_ak74m_2mag_dtk",
+	"rhs_weap_ak74m_2mag_camo_dtk",
+	"rhs_weap_ak74m_camo_pkas",
+	"rhs_weap_ak74m_camo_1p63",
+	"rhs_weap_ak74m_camo_pso1m2",
+	"rhs_weap_ak74m_camo_1p29",
+	"rhs_weap_ak74m_desert_dtk",
+	"rhs_weap_ak74m_plummag_dtk",
+	"rhs_weap_ak74m_gp25_dtk",
+	"rhs_weap_ak74m_gp25_npz_dtk",
+	"rhs_weap_ak74m_gp25_pkas",
+	"rhs_weap_ak74m_gp25_1p63",
+	"rhs_weap_ak74m_gp25_pso1m2",
+	"rhs_weap_ak74m_gp25_1p29",
+	"rhs_weap_svdp_pso1",
+	"rhs_weap_svds_pso1",
+	"rhs_weap_rpg7_pgo"
+
+];
+
+_rhs_magazines = [
+	"rhs_mag_rgd5",
+	"rhs_mag_rdg2_white",
+	"rhs_mag_rdg2_black",
+	"rhs_mag_nspn_yellow",
+	"rhs_mag_nspn_red",
+	"rhs_mag_nspn_green",
+	"rhs_mag_nspd",
+	"rhs_mag_fakel",
+	"rhs_mag_fakels",
+	"rhs_mag_zarya2",
+	"rhs_mag_plamyam",
+
+
+//5.45 mags
+	"rhs_30Rnd_545x39_AK",
+	"rhs_30Rnd_545x39_7N10_AK",
+	"rhs_30Rnd_545x39_7N22_AK",
+	"rhs_30Rnd_545x39_AK_green",
+
+	"rhs_45Rnd_545x39_AK",
+	"rhs_45Rnd_545x39_7N10_AK",
+	"rhs_45Rnd_545x39_7N22_AK",
+	"rhs_45Rnd_545x39_AK_green",
+
+//GP25 ammo
+	"rhs_VOG25",
+	"rhs_VOG25p",
+	"rhs_vg40tb",
+	"rhs_vg40sz",
+	"rhs_vg40op_white",
+	"rhs_vg40op_green",
+	"rhs_vg40op_red",
+	"rhs_GRD40_white",
+	"rhs_GRD40_green",
+	"rhs_GRD40_red",
+	"rhs_GRD50",
+	"rhs_GRD100",
+	"rhs_GRD200",
+
+//7.62 mags
+	"rhs_30Rnd_762x39mm",
+	"rhs_30Rnd_762x39mm_tracer",
+	"rhs_30Rnd_762x39mm_89",
+
+	"rhs_10Rnd_762x54mmR_7N1",
+	"rhs_100Rnd_762x54mmR",
+	"rhs_100Rnd_762x54mmR_green",
+
+//pistol mags
+	"rhs_mag_9x19_17",
+	"rhs_mag_9x18_12_57N181S",
+
+//mines
+	"rhs_mine_tm62m_mag",
+	"rhs_mine_pmn2_mag",
+
+//rpgs
+	"rhs_rpg7_PG7VL_mag",
+	"rhs_rpg7_PG7VR_mag",
+	"rhs_rpg7_OG7V_mag",
+	"rhs_rpg7_TBG7V_mag",
+	"rhs_rpg26_mag",
+	"rhs_rshg2_mag",
+	"rhs_mag_9k38_rocket"
+];
+
+_rhs_items = [
+	"itemRadio",
+	"itemMap",
+	"itemcompass",
+	"itemWatch",
+	//misc items
+	"toolkit",
+	"medikit",
+	"firstaidkit",
+	"MineDetector",
+
+	//uniforms
+	"rhs_uniform_flora",
+	"rhs_uniform_vdv_emr",
+	"rhs_uniform_msv_emr",
+	"rhs_uniform_vdv_flora",
+	"rhs_uniform_vdv_mflora",
+	"rhs_uniform_m88_patchless",
+	"rhs_uniform_flora_patchless",
+	"rhs_uniform_flora_patchless_alt",
+	"rhs_uniform_emr_patchless",
+	"rhs_uniform_mflora_patchless",
+	"rhs_uniform_df15",
+	//"rhs_uniform_gorka_r",
+
+	//vests
+	"rhs_6sh46",
+	//6b13 series
+	"rhs_6b13",
+	"rhs_6b13_6sh92",
+	"rhs_6b13_crewofficer",
+	"rhs_6b13_6sh92_vog",
+	"rhs_6b13_6sh92_headset_mapcase",
+	"rhs_6b13_6sh92_radio",
+	"rhs_6b13_flora",
+	"rhs_6b13_Flora_crewofficer",
+	"rhs_6b13_Flora_6sh92_vog",
+	"rhs_6b13_Flora_6sh92_headset_mapcase",
+	"rhs_6b13_Flora_6sh92_radio",
+	"rhs_6b13_emr",
+	//6b23 series
+	"rhs_6b23",
+	"rhs_6b23_crew",
+	"rhs_6b23_crewofficer",
+	"rhs_vest_commander",
+	"rhs_6b23_engineer",
+	"rhs_6b23_medic",
+	"rhs_6b23_rifleman",
+	"rhs_6b23_sniper",
+	"rhs_6b23_6sh92",
+	"rhs_6b23_6sh92_vog",
+	"rhs_6b23_6sh92_vog_headset",
+	"rhs_6b23_6sh92_headset",
+	"rhs_6b23_6sh92_headset_mapcase",
+	"rhs_6b23_6sh92_radio",
+	//vests digi
+	"rhs_6b23_digi",
+	"rhs_6b23_digi_crew",
+	"rhs_6b23_digi_crewofficer",
+	"rhs_6b23_digi_engineer",
+	"rhs_6b23_digi_medic",
+	"rhs_6b23_digi_rifleman",
+	"rhs_6b23_digi_sniper",
+	"rhs_6b23_digi_6sh92",
+	"rhs_6b23_digi_6sh92_vog",
+	"rhs_6b23_digi_6sh92_vog_headset",
+	"rhs_6b23_digi_6sh92_headset",
+	"rhs_6b23_digi_6sh92_headset_mapcase",
+	"rhs_6b23_digi_6sh92_radio",
+	//mountain flora
+	"rhs_6b23_ML",
+	"rhs_6b23_ML_crew",
+	"rhs_6b23_ML_crewofficer",
+	"rhs_6b23_ML_engineer",
+	"rhs_6b23_ML_medic",
+	"rhs_6b23_ML_rifleman",
+	"rhs_6b23_ML_sniper",
+	"rhs_6b23_ML_6sh92",
+	"rhs_6b23_ML_6sh92_vog",
+	"rhs_6b23_ML_6sh92_vog_headset",
+	"rhs_6b23_ML_6sh92_headset",
+	"rhs_6b23_ML_6sh92_headset_mapcase",
+	"rhs_6b23_ML_6sh92_radio",
+	"rhs_6sh92",
+	"rhs_6sh92_vog",
+	"rhs_6sh92_vog_headset",
+	"rhs_6sh92_headset",
+	"rhs_6sh92_radio",
+	"rhs_6sh92_digi",
+	"rhs_6sh92_digi_vog",
+	"rhs_6sh92_digi_vog_headset",
+	"rhs_6sh92_digi_headset",
+	"rhs_6sh92_digi_radio",
+
+
+	//other
+	"rhs_vydra_3m",
+
+	//headgear
+	"rhs_fieldcap",
+	"rhs_fieldcap_helm",
+	"rhs_tsh4",
+	"rhs_tsh4_ess",
+	"rhs_tsh4_bala",
+	"rhs_tsh4_ess_bala",
+
+	"rhs_zsh7a",
+	"rhs_zsh7a_mike",
+
+	"rhs_gssh18",
+
+	"rhs_6b26",
+	"rhs_6b26_ess",
+	"rhs_6b26_bala",
+	"rhs_6b26_ess_bala",
+
+	"rhs_6b26_green",
+	"rhs_6b26_ess_green",
+	"rhs_6b26_bala_green",
+	"rhs_6b26_ess_bala_green",
+
+	"rhs_6b27m",
+	"rhs_6b27m_ess",
+	"rhs_6b27m_bala",
+	"rhs_6b27m_ess_bala",
+
+	"rhs_6b27m_digi",
+	"rhs_6b27m_digi_ess",
+	"rhs_6b27m_digi_bala",
+	"rhs_6b27m_digi_ess_bala",
+
+	"rhs_6b27m_ml",
+	"rhs_6b27m_ml_ess",
+	"rhs_6b27m_ml_bala",
+	"rhs_6b27m_ml_ess_bala",
+
+	"rhs_6b27m_green",
+	"rhs_6b27m_green_ess",
+	"rhs_6b27m_green_bala",
+	"rhs_6b27m_green_ess_bala",
+
+	"rhs_6b28",
+	"rhs_6b28_ess",
+	"rhs_6b28_bala",
+	"rhs_6b28_ess_bala",
+
+	"rhs_6b28_flora",
+	"rhs_6b28_flora_ess",
+	"rhs_6b28_flora_ess_bala",
+	"rhs_6b28_flora_bala",
+
+	"rhs_6b28_green",
+	"rhs_6b28_green_ess",
+	"rhs_6b28_green_bala",
+	"rhs_6b28_green_ess_bala",
+
+	"rhs_Booniehat_digi",
+	"rhs_Booniehat_flora",
+	"rhs_fieldcap_digi",
+	"rhs_fieldcap_helm_digi",
+	"rhs_fieldcap_ml",
+	"rhs_fieldcap_helm_ml",
+	"rhs_beanie",
+	"rhs_beanie_green",
+	//facewear
+	"rhs_balaclava",
+	"rhs_scarf",
+
+	//accesories
+	"rhs_bipod",
+	"rhs_acc_npz",
+	"rhs_acc_1pn93_1",
+	"rhs_acc_1pn93_2",
+	"rhs_acc_pso1m2",
+	"rhs_acc_pkas",
+	"rhs_acc_pgo7v",
+	"rhs_acc_1p29",
+	"rhs_acc_1p78",
+	"rhs_acc_ekp1",
+	"rhs_acc_1p63",
+	"rhs_acc_tgpa",
+	"rhs_acc_pbs1",
+	"rhs_acc_tgpv",
+	"rhs_acc_dtk4long",
+	"rhs_acc_dtk4short",
+	"rhs_acc_dtk4screws",
+	"rhs_acc_dtk3",
+	"rhs_acc_dtk1",
+	"rhs_acc_dtk",
+	"rhs_acc_dtk1l",
+	"rhs_acc_ak5",
+
+	"O_UavTerminal"
+];
+
+_rhs_backpacks = [
+	"rhs_assault_umbts",
+	"rhs_assault_umbts_engineer",
+	"rhs_assault_umbts_engineer_empty",
+	"rhs_assault_umbts_medic",
+	"rhs_rpg",
+	"rhs_rpg_empty",
+	"rhs_sidor",
+	"rhs_sidorMG",
+	"RHS_NSV_Gun_Bag",
+	"RHS_NSV_Tripod_Bag"
---
+++ Tools and Helpers/Classnames/RHS USAF.h
@@ -0,0 +1,438 @@
+_rhs_weapons = [
+
+	"Binocular",
+
+
+	"rhs_weap_m4",
+	"rhs_weap_m4_m203",
+	"rhs_weap_m4_grip",
+	"rhs_weap_m4_grip2",
+	"rhs_weap_m4_bipod",
+	"rhs_weap_m4_carryhandle",
+	"rhs_weap_m4_carryhandle_pmag",
+	"rhs_weap_m4_m320",
+
+	"rhs_weap_m4a1_m320",
+	"rhs_weap_m4a1_m203",
+	"rhs_weap_m4a1_carryhandle_m203",
+	"rhs_weap_m4a1",
+	"rhs_weap_m4a1_grip",
+	"rhs_weap_m4a1_grip2",
+	"rhs_weap_m4a1_bipod",
+	"rhs_weap_m4a1_carryhandle",
+	"rhs_weap_m4a1_carryhandle_grip",
+	"rhs_weap_m4a1_carryhandle_grip2",
+	"rhs_weap_m4a1_carryhandle_bipod",
+
+	"rhs_weap_m4a1_m203s_wd",
+	"rhs_weap_m4a1_m203s_d",
+	"rhs_weap_m4a1_m203s_sa",
+
+	"rhs_weap_m16a4",
+	"rhs_weap_m16a4_grip",
+	"rhs_weap_m16a4_bipod",
+	"rhs_weap_m16a4_carryhandle",
+	"rhs_weap_m16a4_carryhandle_M203",
+	"rhs_weap_m16a4_carryhandle_pmag",
+	"rhs_weap_m16a4_carryhandle_grip",
+	"rhs_weap_m16a4_carryhandle_grip_pmag",
+	"rhs_weap_m16a4_carryhandle_bipod",
+
+	"rhs_weap_m249_pip_S",
+	"rhs_weap_m249_pip_S_para",
+	"rhs_weap_m249_pip_S_vfg",
+	"rhs_weap_m249_pip_L",
+	"rhs_weap_m249_pip_L_para",
+	"rhs_weap_m249_pip_L_vfg",
+
+	"rhs_weap_m240B",
+	"rhs_weap_m240B_CAP",
+	"rhs_weap_m240G",
+
+	"rhs_weap_m14ebrri",
+	"rhs_weap_sr25",
+	"rhs_weap_sr25_ec",
+
+	"rhs_weap_m39",
+	"rhs_weap_XM2010",
+	"rhs_weap_XM2010_d",
+	"rhs_weap_XM2010_wd",
+	"rhs_weap_XM2010_sa",
+
+	"rhs_weap_m32",
+
+	//"rhs_weap_M107",
+
+	"rhs_weap_M320",
+
+	"rhsusf_weap_m1911a1",
+	"rhsusf_weap_glock17g4",
+
+	"rhs_weap_M136",
+	"rhs_weap_M136_hedp",
+	"rhs_weap_M136_hp",
+	"rhs_weap_fim92",
+	"rhs_weap_fgm148",
+
+	"rhs_weap_M590_8RD",
+	"rhs_weap_M590_5RD",
+
+	// Weap AI
+
+	"rhs_m4_compm4",
+	"rhs_m4_acog",
+	"rhs_m4_acog2",
+	"rhs_m4_acog3",
+	"rhs_m4_grip_compm4",
+	"rhs_m4_grip_acog",
+	"rhs_m4_grip_acog2",
+	"rhs_m4_grip_acog3",
+	"rhs_m4_m320_compm4",
+	"rhs_m4_m320_acog",
+	"rhs_m4_m320_acog2",
+	"rhs_m4_m320_acog3"
+
+];
+
+_rhs_magazines = [
+// Grenades
+	"rhs_mag_m67",
+	"rhs_mag_m69",
+	"rhs_mag_mk84",
+	"rhs_mag_an_m8hc",
+	"rhs_mag_an_m14_th3",
+	"rhs_mag_m7a3_cs",
+	"rhs_mag_mk3a2",
+	"rhs_mag_m18_green",
+	"rhs_mag_m18_purple",
+	"rhs_mag_m18_red",
+	"rhs_mag_m18_yellow",
+
+	"Chemlight_green",
+	"Chemlight_red",
+	"Chemlight_yellow",
+	"Chemlight_blue",
+
+//5.56 mags
+	"rhs_mag_30Rnd_556x45_Mk318_Stanag",
+	"rhs_mag_30Rnd_556x45_Mk262_Stanag",
+	"rhs_mag_30Rnd_556x45_M855A1_Stanag",
+	"rhs_mag_30Rnd_556x45_M855A1_Stanag_No_Tracer",
+	"rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red",
+	"rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Green",
+	"rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Yellow",
+	"rhs_mag_30Rnd_556x45_M200_Stanag",
+
+// Pistol Mags
+	"rhsusf_mag_7x45acp_MHP",
+	"rhsusf_mag_17Rnd_9x19_JHP",
+	"rhsusf_mag_17Rnd_9x19_FMJ",
+
+//M320 ammo
+	"rhs_mag_M441_HE",
+	"rhs_mag_M433_HEDP",
+	"rhs_mag_M781_Practice",
+	"rhs_mag_M4009",
+	"rhs_mag_m576",
+	"rhs_mag_M585_white",
+	"rhs_mag_M661_green",
+	"rhs_mag_M662_red",
+	"rhs_mag_M713_red",
+	"rhs_mag_M714_white",
+	"rhs_mag_M715_green",
+	"rhs_mag_M716_yellow",
+
+	"rhsusf_mag_6Rnd_M433_HEDP",
+	"rhsusf_mag_6Rnd_M781_Practice",
+	"rhsusf_mag_6Rnd_M576_Buckshot",
+	"rhsusf_mag_6Rnd_M441_HE",
+//M249 mag
+	"rhsusf_100Rnd_556x45_soft_pouch",
+	"rhsusf_100Rnd_556x45_M200_soft_pouch",
+	"rhsusf_200Rnd_556x45_soft_pouch",
+
+//M240 mag
+	"rhsusf_50Rnd_762x51",
+	"rhsusf_50Rnd_762x51_m61_ap",
+	"rhsusf_50Rnd_762x51_m62_tracer",
+	"rhsusf_50Rnd_762x51_m80a1epr",
+	"rhsusf_50Rnd_762x51_m82_blank",
+
+	"rhsusf_100Rnd_762x51",
+	"rhsusf_100Rnd_762x51_m61_ap",
+	"rhsusf_100Rnd_762x51_m62_tracer",
+	"rhsusf_100Rnd_762x51_m80a1epr",
+	"rhsusf_100Rnd_762x51_m82_blank",
+
+//M14 mag
+	"rhsusf_20Rnd_762x51_m118_special_Mag",
+	"rhsusf_20Rnd_762x51_m993_Mag",
+
+//xm2010
+	"rhsusf_5Rnd_300winmag_xm2010",
+
+//m107
+	//"rhsusf_10Rnd_STD_50BMG_M107",
+
+//annihilator
+	"rhsusf_5Rnd_00Buck",
+	"rhsusf_8Rnd_00Buck",
+	"rhsusf_5Rnd_Slug",
+	"rhsusf_8Rnd_Slug",
+	"rhsusf_5Rnd_HE",
+	"rhsusf_8Rnd_HE",
+	"rhsusf_5Rnd_FRAG",
+	"rhsusf_8Rnd_FRAG",
+
+//rockets
+	"rhs_m136_mag",
+	"rhs_m136_hedp_mag",
+	"rhs_m136_hp_mag",
+	"rhs_fim92_mag",
+	"rhs_fgm148_magazine_AT",
+
+//explosives
+	"rhsusf_m112_mag",
+	"rhsusf_m112x4_mag",
+
+//mines
+	"rhs_mine_M19_mag"
+];
+
+_rhs_items = [
+
+	//misc items
+	"itemRadio",
+	"itemMap",
+	"itemcompass",
+	"itemWatch",
+	"Binocular",
+	"toolkit",
+	"medikit",
+	"firstaidkit",
+	"MineDetector",
+	"lerca_1200_black",
+	"lerca_1200_tan",
+	"Leupold_Mk4",
+	//nvg
+	"rhsusf_ANPVS_14",
+	"rhsusf_ANPVS_15",
+
+	//uniforms
+	"rhs_uniform_cu_ocp",
+	"rhs_uniform_cu_ucp",
+	"rhs_uniform_FROG01_d",
+	"rhs_uniform_FROG01_wd",
+	"rhs_uniform_FROG01_m81",
+	"rhs_uniform_cu_ocp_82nd",
+	"rhs_uniform_cu_ocp_101st",
+	"rhs_uniform_cu_ocp_10th",
+	"rhs_uniform_cu_ucp_82nd",
+	"rhs_uniform_cu_ucp_101st",
+	"rhs_uniform_cu_ucp_10th",
+	"rhs_uniform_cu_ocp_patchless",
+	"rhs_uniform_cu_ucp_patchless",
+
+	//vests
+	//IOTV
+	"rhsusf_iotv_ocp",
+	"rhsusf_iotv_ocp_Grenadier",
+	"rhsusf_iotv_ocp_Medic",
+	"rhsusf_iotv_ocp_Repair",
+	"rhsusf_iotv_ocp_Rifleman",
+	"rhsusf_iotv_ocp_SAW",
+	"rhsusf_iotv_ocp_Squadleader",
+	"rhsusf_iotv_ocp_teamleader",
+
+	"rhsusf_iotv_ucp",
+	"rhsusf_iotv_ucp_Grenadier",
+	"rhsusf_iotv_ucp_Medic",
+	"rhsusf_iotv_ucp_Repair",
+	"rhsusf_iotv_ucp_Rifleman",
+	"rhsusf_iotv_ucp_SAW",
+	"rhsusf_iotv_ucp_Squadleader",
+	"rhsusf_iotv_ucp_teamleader",
+
+	//SPC
+	"rhsusf_spc",
+	"rhsusf_spc_rifleman",
+	"rhsusf_spc_iar",
+	"rhsusf_spc_corpsman",
+	"rhsusf_spc_crewman",
+	"rhsusf_spc_light",
+	"rhsusf_spc_marksman",
+	"rhsusf_spc_mg",
+	"rhsusf_spc_squadleader",
+	"rhsusf_spc_teamleader",
+
+
+	//opticgear
+	"rhs_googles_black",
+	"rhs_googles_clear",
+	"rhs_googles_yellow",
+	"rhs_googles_orange",
+
+	//headgear
+	"rhsusf_patrolcap_ocp",
+	"rhsusf_patrolcap_ucp",
+	"rhsusf_ach_helmet_ocp",
+	"rhsusf_ach_helmet_ucp",
+	"rhsusf_ach_helmet_m81",
+	"rhsusf_ach_helmet_camo_ocp",
+	"rhsusf_ach_helmet_headset_ocp",
+	"rhsusf_ach_helmet_headset_ucp",
+	"rhsusf_ach_helmet_ess_ocp",
+	"rhsusf_ach_helmet_ess_ucp",
+	"rhsusf_ach_helmet_headset_ess_ocp",
+	"rhsusf_ach_helmet_headset_ess_ucp",
+	"rhsusf_ach_helmet_ocp_norotos",
+	"rhsusf_ach_helmet_ucp_norotos",
+	"rhsusf_ach_bare",
+	"rhsusf_ach_bare_ess",
+	"rhsusf_ach_bare_headset",
+	"rhsusf_ach_bare_headset_ess",
+	"rhsusf_ach_bare_tan",
+	"rhsusf_ach_bare_tan_ess",
+	"rhsusf_ach_bare_tan_headset",
+	"rhsusf_ach_bare_tan_headset_ess",
+	"rhsusf_ach_bare_wood",
+	"rhsusf_ach_bare_wood_ess",
+	"rhsusf_ach_bare_wood_headset",
+	"rhsusf_ach_bare_wood_headset_ess",
+	"rhsusf_ach_bare_des",
+	"rhsusf_ach_bare_des_ess",
+	"rhsusf_ach_bare_des_headset",
+	"rhsusf_ach_bare_des_headset_ess",
+	"rhsusf_ach_bare_semi",
+	"rhsusf_ach_bare_semi_ess",
+	"rhsusf_ach_bare_semi_headset",
+	"rhsusf_ach_bare_semi_headset_ess",
+	"rhsusf_opscore_01",
+	"rhsusf_opscore_01_tan",
+	"rhsusf_opscore_02",
+	"rhsusf_opscore_02_tan",
+	"rhsusf_opscore_03_ocp",
+	"rhsusf_opscore_04_ocp",
+	"rhsusf_cvc_helmet",
+	"rhsusf_cvc_ess",
+	"rhsusf_cvc_green_helmet",
+	"rhsusf_cvc_green_ess",
+	"rhsusf_hgu56p",
+	"rhsusf_hgu56p_mask",
+	"rhs_Booniehat_ocp",
+	"rhs_Booniehat_ucp",
+	"rhs_Booniehat_marpatd",
+	"rhs_Booniehat_marpatwd",
+	"rhs_Booniehat_m81",
+	"rhsusf_Bowman",
+	"rhsusf_Bowman_cap",
+
+	// USMC LWH
+	"rhsusf_lwh_helmet_marpatd",
+	"rhsusf_lwh_helmet_marpatd_ess",
+	"rhsusf_lwh_helmet_marpatd_headset",
+	"rhsusf_lwh_helmet_marpatwd",
+	"rhsusf_lwh_helmet_marpatwd_ess",
+	"rhsusf_lwh_helmet_marpatwd_headset",
+
+	// USMC MICH 2000
+	"rhsusf_mich_helmet_marpatwd",
+	"rhsusf_mich_helmet_marpatwd_headset",
+	"rhsusf_mich_helmet_marpatd",
+	"rhsusf_mich_helmet_marpatd_headset",
+	"rhsusf_mich_helmet_marpatwd_alt",
+	"rhsusf_mich_helmet_marpatwd_alt_headset",
+	"rhsusf_mich_helmet_marpatd_alt",
+	"rhsusf_mich_helmet_marpatd_alt_headset",
+	"rhsusf_mich_helmet_marpatwd_norotos",
+	"rhsusf_mich_helmet_marpatwd_norotos_headset",
+	"rhsusf_mich_helmet_marpatd_norotos",
+	"rhsusf_mich_helmet_marpatd_norotos_headset",
+	"rhsusf_mich_helmet_marpatwd_norotos_arc",
+	"rhsusf_mich_helmet_marpatwd_norotos_arc_headset",
+	"rhsusf_mich_helmet_marpatd_norotos_arc",
+	"rhsusf_mich_helmet_marpatd_norotos_arc_headset",
+	"rhsusf_mich_bare",
+	"rhsusf_mich_bare_headset",
+	"rhsusf_mich_bare_alt",
+	"rhsusf_mich_bare_norotos_alt",
+	"rhsusf_mich_bare_norotos_alt_headset",
+	"rhsusf_mich_bare_norotos_arc_alt",
+	"rhsusf_mich_bare_norotos_arc_alt_headset",
+	"rhsusf_mich_bare_norotos",
+	"rhsusf_mich_bare_norotos_headset",
+	"rhsusf_mich_bare_norotos_arc",
+	"rhsusf_mich_bare_norotos_arc_headset",
+	"rhsusf_mich_bare_tan",
+	"rhsusf_mich_bare_tan_headset",
+	"rhsusf_mich_bare_alt_tan",
+	"rhsusf_mich_bare_norotos_tan",
+	"rhsusf_mich_bare_norotos_tan_headset",
+	"rhsusf_mich_bare_norotos_alt_tan",
+	"rhsusf_mich_bare_norotos_alt_tan_headset",
+	"rhsusf_mich_bare_norotos_arc_tan",
+	"rhsusf_mich_bare_norotos_arc_tan_headset",
+	"rhsusf_mich_bare_norotos_arc_alt_tan",
+	"rhsusf_mich_bare_norotos_arc_alt_tan_headset",
+	"rhsusf_mich_bare_semi",
+	"rhsusf_mich_bare_semi_headset",
+	"rhsusf_mich_bare_alt_semi",
+	"rhsusf_mich_bare_norotos_semi",
+	"rhsusf_mich_bare_norotos_semi_headset",
+	"rhsusf_mich_bare_norotos_alt_semi",
+	"rhsusf_mich_bare_norotos_alt_semi_headset",
+	"rhsusf_mich_bare_norotos_arc_semi",
+	"rhsusf_mich_bare_norotos_arc_semi_headset",
+	"rhsusf_mich_bare_norotos_arc_alt_semi",
+	"rhsusf_mich_bare_norotos_arc_alt_semi_headset",
+
+	//accesories
+	"rhsusf_acc_compm4",
+	"rhsusf_acc_LEUPOLDMK4",
+	"rhsusf_acc_LEUPOLDMK4_2",
+	"rhsusf_acc_HAMR",
+	"rhsusf_acc_EOTECH",
+	"rhsusf_acc_eotech_552",
+	"rhsusf_acc_ELCAN",
+	"rhsusf_acc_ELCAN_PIP",
+	"rhsusf_acc_ELCAN_ard",
+	"rhsusf_acc_ACOG",
+	"rhsusf_acc_ACOG_PIP",
+	"rhsusf_acc_ACOG2",
+	"rhsusf_acc_ACOG3",
+	"rhsusf_acc_ACOG3_test",
+	"rhsusf_acc_ACOG_USMC",
+	"rhsusf_acc_ACOG2_USMC",
+	"rhsusf_acc_ACOG3_USMC",
+	"rhsusf_acc_M2010S",
+	"rhsusf_acc_SR25S",
+	"rhsusf_acc_anpeq15",
+	"rhsusf_acc_anpeq15_light",
+	"rhsusf_acc_anpeq15side",
+	"rhsusf_acc_anpeq15A",
+	"rhsusf_acc_harris_bipod",
+	"rhsusf_acc_rotex5_grey",
+	"rhsusf_acc_rotex5_tan",
+	"rhsusf_acc_nt4_black",
+	"rhsusf_acc_nt4_tan",
+	"rhsusf_acc_SF3P556",
+	"rhsusf_acc_SFMB556",
+	"rhsusf_acc_M2A1"
+
+];
+
+_rhs_backpacks = [
+	"rhsusf_assault_eagleaiii_ucp",
+	"rhsusf_assault_eagleaiii_ocp",
+	"rhsusf_assault_eagleaiii_coy",
+	"rhsusf_falconii",
+
+	//weapon bags
+	"B_rhsusf_B_BACKPACK",
+	"RHS_M2_Gun_Bag",
+	"RHS_M2_Tripod_Bag",
+	"RHS_M2_MiniTripod_Bag",
+	"RHS_Mk19_Gun_Bag",
+	"RHS_Mk19_Tripod_Bag"
+
+];
